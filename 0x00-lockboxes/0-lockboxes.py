a7d8022982135e00af8eea17d69b3341a04a091c
eNobreg <ezra.nobrega@outlook.com>
2020-07-19 16:06:37 -0400

    [New] Initial push

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+ Lockboxes Program solution
+"""
+
+def canUnlockAll(boxes):
+    """
+    Bool function to see if boxes can be opened
+    """
+    if not boxes:
+        return False
+    seen_boxes = {0}
+    keys = [0]
+    while(keys):
+        c_key = keys.pop()
+        for key in boxes[c_key]:
+            if key not in seen_boxes:
+                seen_boxes.add(key)
+                keys.append(key)
+    
+    for i in range(len(boxes)):
+        if i not in seen_boxes:
+            return False
+    return(True)
+    
