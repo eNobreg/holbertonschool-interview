7171674c8077d9f37a950f88b1594020e06256f7
eNobreg <ezra.nobrega@outlook.com>
2020-08-09 15:02:59 -0400

    [Update] Fixed pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
+
 def minOperations(n):
-    
+
     currentChars = 1
     copyChars = 1
     counter = 1
-    
-    if (n < 2)
-        return (0);
-    
+
+    if (n < 2):
+        return (0)
+
     if (currentChars == 1):
         currentChars += copyChars
         counter += 1
-    
+
     while (currentChars < n):
         if (n % currentChars == 0 and copyChars + currentChars <= n):
             copyChars = currentChars
@@ -20,6 +21,6 @@ def minOperations(n):
             currentChars += copyChars
         else:
             currentChars += copyChars
-        counter +=1
-            
-    return (counter);
+        counter += 1
+
+    return (counter)
