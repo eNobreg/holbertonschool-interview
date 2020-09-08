151d2d0fb738209257e656d4a6aa81c4a5fae2eb
eNobreg <ezra.nobrega@outlook.com>
2020-09-08 14:57:56 -0400

    [New] Initial push

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+ * check_cycle - Checks for a loop in a linked list
+ * @list: The list to check
+ * Return: 0 on no cycle, 1 on cycle
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *slow  = list;
+    listint_t *fast = list;
+
+    while (fast && slow && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return(1);
+    }
+    return(0);
+}
