9e45c2a2d0b8f3d624b007b15154645b46cf92df
eNobreg <ezra.nobrega@outlook.com>
2020-10-07 23:09:44 -0400

    Updated to work

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,45 +1,62 @@
 #include "menger.h"
 
+int divide_sponge(int row, int col);
+
+/**
+ * menger - Print a menger square based on a level
+ * @level: The level of menger sponge to print
+ * Return: Nothing
+ */
 void menger(int level)
 {
-    int i, j;
-    int index_i = 0;
-    int index_j = 0;
-    int scale = 0;
-    int size = 0;
-    char level1[3][3] = {
-		{'#', '#', '#'},
-		{'#', ' ', '#'},
-		{'#', '#', '#'}
-	};
-    
+	int i, j, size;
 
-    size = pow(3, level);
-    scale = size / 3;
+	size = pow(3, level);
 
-    if (level == 0)
-    {
-        putchar('#');
-        return;
-    }
+	if (level == 0)
+	{
+		putchar('#');
+		return;
+	}
 
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            index_i = i / scale;
-            index_j = j / scale;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (divide_sponge(i, j) == 1)
+				putchar(' ');
+			else
+			{
+				putchar('#');
+			}
+		}
+		putchar('\n');
+	}
+}
 
-            if(level1[index_i][index_j] == ' ')
-                putchar(' ');
-            else if (i % 3 == 1 && j % 3 == 1)
-                putchar(' ');
-            else
-            {
-                putchar('#');
-            }
-        }
-        putchar('\n');
-    }
+/**
+ * divide_sponge - Recursive function to detect if a space possible
+ * @row: The row to check
+ * @col: The col to check
+ * Return: 1 if it could be a space, 0 if it couldn't
+ */
+int divide_sponge(int row, int col)
+{
+	int result = 0;
+
+	if (row || col)
+	{
+		if (row % 3 == 1 && col % 3 == 1)
+		{
+			return (1);
+		}
+		else
+		{
+			result = divide_sponge(row / 3, col / 3);
+		}
+		if (result == 1)
+			return (1);
+	}
+	return (0);
 }
