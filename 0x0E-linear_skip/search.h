c0d8d887db738afd53435a6243b25d5e22691afd
eNobreg <ezra.nobrega@outlook.com>
2020-11-10 16:28:31 -0500

    [Fix] Small fixes

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -1,11 +1,10 @@
-#ifndef _SEARCH_H_
-#define _SEARCH_H_
+#ifndef SEARCH_H
+#define SEARCH_H
 
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <math.h>
 
 
 /**
@@ -27,9 +26,6 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
-skiplist_t *create_skiplist(int *array, size_t size);
-void print_skiplist(const skiplist_t *list);
-void free_skiplist(skiplist_t *list);
 skiplist_t *linear_skip(skiplist_t *head, int value);
 skiplist_t *traverse(skiplist_t *last_node, int value);
 
