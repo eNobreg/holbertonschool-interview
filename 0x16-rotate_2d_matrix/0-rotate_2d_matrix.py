fd72868e8b8fa24294d2abe55fa3e71ad44ce2c2
root <root@Smallatron.localdomain>
2021-01-20 18:12:02 -0500

    [Initial] First push for all files

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Rotation module
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotation function
+    """
+
+    N = len(matrix)
+    column = N - 1
+
+    for x in range(0, int(N / 2)):
+        for y in range(x, column - x):
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[y][column - x]
+            matrix[y][column - x] = matrix[column - x][column - y]
+            matrix[column - x][column - y] = matrix[column - y][x]
+            matrix[column - y][x] = temp
