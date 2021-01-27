2c712d49feaff1e53055945e18234f9d4f64f8d4
root <root@Smallatron.localdomain>
2021-01-27 14:59:21 -0500

    [Initial] First push for directory

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * find_listint_loop - Finds a loop in a linked list
+ * @head: The start of the list
+ * Return: Null for no loop, or address of the looping node
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = slow->next->next;
+
+		if (slow == fast)
+		{
+			return (slow);
+		}
+	}
+	return (NULL);
+}
---
+++ 0x17-find_the_loop/README.md
@@ -0,0 +1,4 @@
+# Find the Loop
+A project for finding the loop in a linked list in C
+
+The solution is to have two pointers, one slow and one fast, if those pointers are ever equal to each other there is a loop
